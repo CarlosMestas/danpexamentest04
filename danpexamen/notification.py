d53e37a5ab2078fbba2e037696038e7d59f044d3
Araceli Quispe <maraceliquispey@gmail.com>
2022-07-12 16:03:55 -0500

    cambios form

--- danpexamen/notification.py
+++ danpexamen/notification.py
@@ -3,5 +3,5 @@ from django import forms
 
 class InputForm(forms.Form):
     title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': "input"}))
-    text = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': "input"}))
+    text = forms.CharField(max_length=200, widget=forms.Textarea(attrs={'class': "input", 'rows': '5', 'cols': '90', 'maxlength': '200'}))
 
