a25fc5790898e231bb5e1b109c2ad1670570a5c9
Araceli Quispe <maraceliquispey@gmail.com>
2022-07-12 15:49:11 -0500

    Estilos

--- danpexamen/notification.py
+++ danpexamen/notification.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class InputForm(forms.Form):
-    title = forms.CharField(max_length=200)
-    text = forms.CharField(max_length=200)
+    title = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': "input"}))
+    text = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': "input"}))
 
