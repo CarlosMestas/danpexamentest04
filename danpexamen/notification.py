eb71de38246e6c64d21e7f7f57253513d743dafe
CarlosMestas <42045414+CarlosMestas@users.noreply.github.com>
2022-07-12 02:05:47 -0500

    fcm

--- danpexamen/notification.py
+++ danpexamen/notification.py
@@ -3,5 +3,5 @@ from django import forms
 
 class InputForm(forms.Form):
     title = forms.CharField(max_length=200)
-    info = forms.CharField(max_length=200)
+    text = forms.CharField(max_length=200)
 
