acbe57a70c1bb1e68d4cb6370cfc27c6ae1c8d57
CarlosMestas <42045414+CarlosMestas@users.noreply.github.com>
2022-07-12 02:34:22 -0500

    fcm

--- danpexamen/views.py
+++ danpexamen/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .notification import InputForm
 from django.http import HttpResponse
-
+from pusher_push_notifications import PushNotifications
 
 def notification_view(request):
     context = {}
@@ -13,7 +13,6 @@ def home(request):
     return HttpResponse('Hello World')
 
 def push_notification(request):
-    from pusher_push_notifications import PushNotifications
     pn_client = PushNotifications(
         instance_id='b6dd6a14-bd22-4988-9af4-0e17301cd078',
         secret_key='947034E6EE915B5FE14519DE5DA8AA71E4C1B4C3092AA1156A481D8E3D461AB6',
